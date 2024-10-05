aa61b8c3070f74b946877d86ef1a8bfd96fa5d6b
dchernyavsky <dchernyavsky678@gmail.com>
2024-10-05 14:15:18 +0500

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import crypto from "crypto";
+import fs from "fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = `${import.meta.dirname}/files/fileToCalculateHashFor.txt`;
+  const hash = crypto.createHash("sha256");
+  const readStream = fs.createReadStream(filePath);
+
+  readStream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  readStream.on("end", () => {
+    console.log(hash.digest("hex"));
+  });
 };
 
 await calculateHash();
