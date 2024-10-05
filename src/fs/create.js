507ccce78ad4f41b7f8cf51ff3c71b734fc7d5c3
dchernyavsky <dchernyavsky678@gmail.com>
2024-10-05 10:59:04 +0500

    feat: fs

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,12 @@
+import fs from "fs";
+
 const create = async () => {
-    // Write your code here 
+  const filePath = "src/fs/files/fresh.txt";
+  const fileContent = "I am fresh and young";
+  if (fs.existsSync(filePath)) {
+    throw new Error("FS operation failed");
+  }
+  fs.writeFileSync(filePath, fileContent);
 };
 
 await create();
