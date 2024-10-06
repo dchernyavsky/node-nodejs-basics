a711b5c53ed57cb8ea23b9887c6a3ad993fa610c
dchernyavsky <dchernyavsky678@gmail.com>
2024-10-06 16:32:20 +0500

    feat: wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,22 @@
+import os from "os";
+import { Worker } from "worker_threads";
+
 const performCalculations = async () => {
-    // Write your code here
+  const results = await Promise.all(
+    os.cpus().map(
+      (_, index) =>
+        new Promise((resolve) => {
+          const worker = new Worker(`${import.meta.dirname}/worker.js`, {
+            workerData: index + 10,
+          });
+          worker.on("message", (data) => {
+            resolve(data);
+            worker.terminate();
+          });
+        })
+    )
+  );
+  console.log(results);
 };
 
 await performCalculations();
